1c5795a5ac624ef9658450c54f312e6f107b0371
Tobias Carlander <tobias.carlander@wfp.org>
2012-11-13 13:29:53 +0400

    update for smartptt

--- tracking_push_update_moto.py
+++ tracking_push_update_moto.py
@@ -18,23 +18,29 @@ def ConfigSectionMap(section):
     return dict1
 
 Config = ConfigParser.ConfigParser()
-Config.read("tracking_moto.ini")
+Config.read("tracking_settings.ini")
 settings = ConfigSectionMap("Settings")
-print settings
+
 trackMeServer = ConfigSectionMap("Settings")['trackme_server']
-wave_server = ConfigSectionMap("Settings")['wave_server']
-wave_server_port = ConfigSectionMap("Settings")['wave_server_port']
 foodsat_prefix = ConfigSectionMap("Settings")['foodsat_prefix']
-motoFile =  settings['motofile']
 
-listOfServers = [
-    wave_server +':'+wave_server_port
-    ]
+system_type =  settings['system_type']
+
+if system_type == 'wave':
+    wave_server_address = ConfigSectionMap("Settings")['wave_server']
+    wave_server_port = ConfigSectionMap("Settings")['wave_server_port']
+    server_url =  wave_server_address +':'+wave_server_port
+
+if system_type == 'smartptt_db'or system_type == 'trbonet_db':
+    db_name=ConfigSectionMap("Settings")['wave_server']
+
+if system_type == 'trbonet' or system_type == 'smartptt':
+    kml_file =  settings['kml_file']
 
 
-def update(dataTable):
+def update_trbonet(dataTable):
     outputData = []
-    p = re.compile('\d+)
+    p = re.compile('\d+')
     ptime =re.compile('\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d')
     #print dataTable
     for i in dataTable:
@@ -46,31 +52,104 @@ def update(dataTable):
         coord = i.getElementsByTagName('coordinates')[0].childNodes[0].nodeValue
         devicePosition['Lon'] = coord.split(',')[0]
         devicePosition['Lat'] = coord.split(',')[1]
-        devicePosition['ID']=p.findall(  i.getElementsByTagName('name')[0].childNodes[0].nodeValue)[0]
+#        devicePosition['ID']=p.findall(  i.getElementsByTagName('name')[0].childNodes[0].nodeValue)[0]
         
-        pushUrl = "/trackme/requests.php?a=upload&id=%s%s&lat=%s&long=%s&do=%s&tn="%(foodsat_prefix,devicePosition['ID'],devicePosition['Lat'],devicePosition['Lon'],devicePosition['Timestamp'])
-        #try:
-        import urllib
-        pushUrl = urllib.quote(pushUrl,'\&/?=')
-        print 'http://'+trackMeServer+ pushUrl
-        
-        urllib2.urlopen('http://'+trackMeServer+ pushUrl)
-        #except:
-        outputData.append(devicePosition)
+        send_data(devicePosition)   
 
-for server in listOfServers:
+        
+
+def update_smartptt_kml(dataTable):
+
+    p = re.compile('\[(\d+)\]')
+    r = re.compile('(\d+)')
+    for i in dataTable:
+        timetable = i.getElementsByTagName('description')[0].childNodes[0].nodeValue
+        devicePosition = {}
+        devicePosition['Timestamp']=  timetable
+
+        try:
+            devicePosition['ID']=p.findall(  i.getElementsByTagName('name')[0].childNodes[0].nodeValue)[0]
+        except:
+            devicePosition['ID']=r.findall(  i.getElementsByTagName('name')[0].childNodes[0].nodeValue)[0]
+            
+        coord = i.getElementsByTagName('coordinates')[0].childNodes[0].nodeValue
+        devicePosition['Lon'] = coord.split(',')[0]
+        devicePosition['Lat'] = coord.split(',')[1]
+#        devicePosition['ID']= i.getElementsByTagName('name')[0].childNodes[0].nodeValue
+        
+        send_data(devicePosition)   
+
+        
+
+def update_wave(dataTable):
+    p = re.compile('\((\d+)\)')
+    r = re.compile('#(\d+)')
+    for i in dataTable:
+        timetable = i.getElementsByTagName('value')[0].childNodes[0].nodeValue
+        devicePosition = {}
+        devicePosition['Name'] = i.getElementsByTagName('name')[0].childNodes[0].nodeValue
+        coord = i.getElementsByTagName('coordinates')[0].childNodes[0].nodeValue
+        devicePosition['Lon'] = coord.split(',')[0]
+        devicePosition['Lat'] = coord.split(',')[1]
+        try:
+            devicePosition['ID']=p.findall(  i.getElementsByTagName('name')[0].childNodes[0].nodeValue)[0]
+        except:
+            devicePosition['ID']=r.findall(  i.getElementsByTagName('name')[0].childNodes[0].nodeValue)[0]
+        devicePosition['Timestamp']= datetime.datetime.fromtimestamp(float(timetable))
+       
+        send_data(devicePosition)   
+
+
+def update_kml_file(dataTable):
+
+    if system_type == 'trbonet':
+        update_trbonet(dataTable)
+    if system_type == 'smartptt':
+        update_smartptt_kml(dataTable)
+                
+
+def send_data(devicePosition):
+        pushUrl = "/trackme/requests.php?a=upload&id=%s%s&lat=%s&long=%s&do=%s&tn="%(foodsat_prefix,devicePosition['ID'],devicePosition['Lat'],devicePosition['Lon'],devicePosition['Timestamp'])
+        import urllib
+        #print pushUrl
+        pushUrl = urllib.quote(pushUrl,'\&/?=')
+        try:
+            urllib2.urlopen('http://'+trackMeServer+ pushUrl)
+        except Exception as e:
+            print e            
+
+
+if system_type == 'trbonet' or system_type == 'smartptt':
     try:
-        fh = open(motoFile,'r')
+        fh = open(kml_file,'r')
         data = fh.read()
         soup = parseString(data)
         fh.close()
         devicePosition = dict()
         dataTable = soup.getElementsByTagName('Placemark')
-        #print dataTable
-        update(dataTable)
+        update_kml_file(dataTable)
     except Exception as e:
         print e
 
-#urllib2.urlopen('http://'+trackMeServer+'/trackme/trackme/update/')
 
+if system_type == 'wave':
+    try:
+        fh = urllib2.urlopen('http://'+ server_url +'/all-subscribers.kml')
+        html = fh.read()
+        fh.close()
+        soup = parseString(html)
+        devicePosition = dict();
+        dataTable = soup.getElementsByTagName('Placemark')
+        update_wave(dataTable)
+    except Exception as e:
+        print e
+        
+if system_type == 'smartptt_db':
+    print "not yet implemented"
+
+if system_type == 'trbonet_db':
+    print "not yet implemented"
+
+
+urllib2.urlopen('http://'+trackMeServer+'/trackme/trackme/update/')
 print 0
