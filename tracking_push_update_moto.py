6e19de181aa4fff832d7123d4ca443d42a85563b
Tobias Carlander <tobias.carlander@wfp.org>
2012-11-20 15:02:39 +0400

    Added fix for empty radio configs & init of remote logging

--- tracking_push_update_moto.py
+++ tracking_push_update_moto.py
@@ -4,6 +4,10 @@ import urllib2, re, datetime, ConfigParser,time
 from xml.dom.minidom import parse, parseString
 from xml.dom import Node
 
+def report_issues(issue):
+    # make connection to trackme server to report any issues
+    print issue
+
 def ConfigSectionMap(section):
     dict1 = {}
     options = Config.options(section)
@@ -23,12 +27,11 @@ settings = ConfigSectionMap("Settings")
 
 trackMeServer = ConfigSectionMap("Settings")['trackme_server']
 foodsat_prefix = ConfigSectionMap("Settings")['foodsat_prefix']
-
 system_type =  settings['system_type']
 
 if system_type == 'wave':
-    wave_server_address = ConfigSectionMap("Settings")['wave_server']
-    wave_server_port = ConfigSectionMap("Settings")['wave_server_port']
+    wave_server_address = ConfigSectionMap("Settings")['radio_server_address']
+    wave_server_port = ConfigSectionMap("Settings")['radio_server_port']
     server_url =  wave_server_address +':'+wave_server_port
 
 if system_type == 'smartptt_db'or system_type == 'trbonet_db':
@@ -52,11 +55,9 @@ def update_trbonet(dataTable):
         coord = i.getElementsByTagName('coordinates')[0].childNodes[0].nodeValue
         devicePosition['Lon'] = coord.split(',')[0]
         devicePosition['Lat'] = coord.split(',')[1]
-#        devicePosition['ID']=p.findall(  i.getElementsByTagName('name')[0].childNodes[0].nodeValue)[0]
         
         send_data(devicePosition)   
 
-        
 
 def update_smartptt_kml(dataTable):
 
@@ -71,12 +72,9 @@ def update_smartptt_kml(dataTable):
             devicePosition['ID']=p.findall(  i.getElementsByTagName('name')[0].childNodes[0].nodeValue)[0]
         except:
             devicePosition['ID']=r.findall(  i.getElementsByTagName('name')[0].childNodes[0].nodeValue)[0]
-            
         coord = i.getElementsByTagName('coordinates')[0].childNodes[0].nodeValue
         devicePosition['Lon'] = coord.split(',')[0]
         devicePosition['Lat'] = coord.split(',')[1]
-#        devicePosition['ID']= i.getElementsByTagName('name')[0].childNodes[0].nodeValue
-        
         send_data(devicePosition)   
 
         
@@ -85,20 +83,22 @@ def update_wave(dataTable):
     p = re.compile('\((\d+)\)')
     r = re.compile('#(\d+)')
     for i in dataTable:
-        timetable = i.getElementsByTagName('value')[0].childNodes[0].nodeValue
-        devicePosition = {}
-        devicePosition['Name'] = i.getElementsByTagName('name')[0].childNodes[0].nodeValue
-        coord = i.getElementsByTagName('coordinates')[0].childNodes[0].nodeValue
-        devicePosition['Lon'] = coord.split(',')[0]
-        devicePosition['Lat'] = coord.split(',')[1]
         try:
-            devicePosition['ID']=p.findall(  i.getElementsByTagName('name')[0].childNodes[0].nodeValue)[0]
-        except:
-            devicePosition['ID']=r.findall(  i.getElementsByTagName('name')[0].childNodes[0].nodeValue)[0]
-        devicePosition['Timestamp']= datetime.datetime.fromtimestamp(float(timetable))
+            timetable = i.getElementsByTagName('value')[0].childNodes[0].nodeValue
+            devicePosition = {}
+            devicePosition['Name'] = i.getElementsByTagName('name')[0].childNodes[0].nodeValue
+            coord = i.getElementsByTagName('coordinates')[0].childNodes[0].nodeValue
+            devicePosition['Lon'] = coord.split(',')[0]
+            devicePosition['Lat'] = coord.split(',')[1]
+            try:
+                devicePosition['ID']=p.findall(  i.getElementsByTagName('name')[0].childNodes[0].nodeValue)[0]
+            except:
+                devicePosition['ID']=r.findall(  i.getElementsByTagName('name')[0].childNodes[0].nodeValue)[0]
+            devicePosition['Timestamp']= datetime.datetime.fromtimestamp(float(timetable))
        
-        send_data(devicePosition)   
-
+            send_data(devicePosition)
+        except Exception as e:
+            report_issues( e )
 
 def update_kml_file(dataTable):
 
@@ -116,7 +116,7 @@ def send_data(devicePosition):
         try:
             urllib2.urlopen('http://'+trackMeServer+ pushUrl)
         except Exception as e:
-            print e            
+            report_issues( e )
 
 
 if system_type == 'trbonet' or system_type == 'smartptt':
@@ -129,7 +129,7 @@ if system_type == 'trbonet' or system_type == 'smartptt':
         dataTable = soup.getElementsByTagName('Placemark')
         update_kml_file(dataTable)
     except Exception as e:
-        print e
+        report_issues( e )
 
 
 if system_type == 'wave':
@@ -142,10 +142,11 @@ if system_type == 'wave':
         dataTable = soup.getElementsByTagName('Placemark')
         update_wave(dataTable)
     except Exception as e:
-        print e
+        report_issues( e )
         
 if system_type == 'smartptt_db':
     print "not yet implemented"
+    #connect to db using python
 
 if system_type == 'trbonet_db':
     print "not yet implemented"
